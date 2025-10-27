062378067173ec09c784bc7b623995fdc3cb981c
panjaripathak16-prog <panjaripathak16@gmail.com>
2025-10-27 15:32:41 +0530

    Add files via upload

---
+++ Decimal to binary.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void decimalToBinary(int n) {
+    if (n == 0)
+        return;
+    decimalToBinary(n / 2);
+    printf("%d", n % 2);
+}
+
+int main() {
+    int num;
+    printf("Enter a decimal number: ");
+    scanf("%d", &num);
+
+    if (num == 0)
+        printf("Binary: 0");
+    else {
+        printf("Binary: ");
+        decimalToBinary(num);
+    }
+
+    return 0;
+}
+
